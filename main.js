a8f345f4b98f59c2c05b5505b94113f4485f40e2
viswanathsoureesh <73836557+viswanathsoureesh@users.noreply.github.com>
2021-07-24 12:42:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,58 +1,41 @@
+noseX=0;
+noseY=0;
+difference=0;
+leftwristX=0;
+rightwristX=0;
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(500,500);
+    video.position(50,200)
 
-var last_position_of_x, last_position_of_y;
+    canvas=createCanvas(500,500);
+    canvas.position(560,200);
 
-    canvas = document.getElementById('myCanvas');
-    ctx = canvas.getContext("2d");
-    
-    color = "black";
-    width_of_line = 2;
+    pose=ml5.poseNet(video,modelloaded);
+    pose.on('pose',gotposes);
+}
 
-var width=screen.width;
+function modelloaded(){
+    console.log("Model Loaded");
+}
 
-var height=screen.height;
+function gotposes(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("nosex="+noseX+" nosey="+noseY);
+        leftwristX=results[0].pose.leftWrist.x;
+        rightwristX=results[0].pose.rightWrist.x;
+        console.log("rightwristX="+rightwristX+" leftwristX="+leftwristX);
+        difference=floor(leftwristX-rightwristX)
+        }
+}
 
-new_width=screen.width-70;
-
-new_height=screen.height-300;
-
-if (width<992){
-    document.getElementById("myCanvas").width=new_width;
-    document.getElementById("myCanvas").height=new_height;
-    document.body.style.overflow="hidden";}
-    canvas.addEventListener("touchstart", my_touchstart);
-    
-    function my_touchstart(e)
-    {
-    
-last_position_of_x=e.touches[0].clientX-canvas.offsetLeft;
-last_position_of_y=e.touches[0].clientY-canvas.offsetTop;
-    }
-
-    canvas.addEventListener("touchmove", my_touchmove);
-    function my_touchmove(e)
-    {
-
-         current_position_of_mouse_x = e.touches[0].clientX - canvas.offsetLeft;
-         current_position_of_mouse_y = e.touches[0].clientY - canvas.offsetTop;
-
-       
-        ctx.beginPath();
-        ctx.strokeStyle = color;
-        ctx.lineWidth = width_of_line;
-
-        console.log("Last position of x and y coordinates = ");
-        console.log("x = " + last_position_of_x + "y = " + last_position_of_y);
-        ctx.moveTo(last_position_of_x, last_position_of_y);
-
-        console.log("Current position of x and y coordinates = ");
-        console.log("x  = " + current_position_of_mouse_x + "y = " + current_position_of_mouse_y);
-        ctx.lineTo(current_position_of_mouse_x, current_position_of_mouse_y);
-        ctx.stroke();
-     
-
-        last_position_of_x = current_position_of_mouse_x; 
-        last_position_of_y = current_position_of_mouse_y;
-    }
-function clear(){
-    ctx.clearRect(0,0,canvas.width,canvas.height);
+function draw(){
+    background('#393E46');
+    fill ("#FDF6F0");
+    stroke ("#FDF6F0");
+    square (noseX,noseY,difference);
+    document.getElementById("square_sides").innerHTML="Width and height of the square ="+difference+"px"
 }
